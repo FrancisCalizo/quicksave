f2ddf67c3540cf84c1ae1b4ba5fd431a0e104f63
Francis Calizo <francisacalizo@gmail.com>
2022-01-10 15:38:05 -0500

    feat: connect to ElephantSQL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,29 @@
-const Pool = require('pg').Pool;
+const ClientClass = require('pg').Client;
 
-const pool = new Pool({
-  user: 'postgres',
-  password: 'password',
-  host: 'localhost',
-  port: 5432,
-  database: 'quicksave',
-});
+const pgUrl =
+  'postgres://tolabgxv:wCI6ydvOK26S-SsHFrssnevZ62qgeyVL@castor.db.elephantsql.com/tolabgxv';
+const client = new ClientClass(pgUrl);
 
-module.exports = pool;
+(async function connect(client) {
+  try {
+    await client.connect();
+  } catch (ex) {
+    console.log('Some error' + ex);
+  }
+})(client);
+
+module.exports = client;
+
+// Note: I was using Pool instead of Client before from a video tutorial. It
+// seems to used more, but I couldn't connect to ElephantSQL using it, I could
+// only connext to it using Client.
+
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'password',
+//   host: 'postgres://tolabgxv:wCI6ydvOK26S-SsHFrssnevZ62qgeyVL@castor.db.elephantsql.com/tolabgxv',
+//   port: 5432,
+//   database: 'quicksave',
+// });
+// module.exports = pool;
