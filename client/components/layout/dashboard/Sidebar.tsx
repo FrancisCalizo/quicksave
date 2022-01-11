ce73856818b51663e055976360f2eedfa59fd075
francis <francis@aahcs.com>
2022-01-10 22:30:28 -0500

    feat: begin implementing sidebar dashboard links

--- client/components/layout/dashboard/Sidebar.tsx
+++ client/components/layout/dashboard/Sidebar.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+import { Flex, Link, Heading, Box } from '@chakra-ui/react';
+
+import { SIDEBAR_LINKS } from 'components/routes';
 
 export default function Sidebar() {
   return (
     <Flex
       w={['100%', '100%', '10%', '15%', '15%']}
-      flexDir="column"
-      alignItems="center"
+      direction="column"
+      justify="space-between"
+      align="center"
       bgGradient="linear(to-b, green.500, green.400)"
       color="#fff"
+      py={4}
     >
-      <h3>Sidebar coming soon...</h3>
+      <Heading as="h3">QuickSave.</Heading>
+
+      <Flex direction="column" w="100%">
+        {SIDEBAR_LINKS.map((link, key) => (
+          <Link key={key} m={5} as={NextLink} href={link.route}>
+            <Box as="button" py={3} w="100%" textAlign="left" _hover={{ bg: 'green.600' }}>
+              {link.title}
+            </Box>
+          </Link>
+        ))}
+      </Flex>
+
+      <div />
     </Flex>
   );
 }
---
+++ client/components/routes.ts
@@ -0,0 +1,6 @@
+export const SIDEBAR_LINKS = [
+  { title: 'Overview', route: '/' },
+  { title: 'Income', route: '/income' },
+  { title: 'Recurring', route: '/recurring' },
+  { title: 'Nonrecurring', route: '/nonrecurring' },
+];
