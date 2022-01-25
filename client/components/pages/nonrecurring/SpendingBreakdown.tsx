ce684eef3c18eda2ddd1ca8e3d4fc61d5afd8e98
francis <francis@aahcs.com>
2022-01-25 08:58:29 -0500

    fix: remove legend for expense, fix data labels

--- client/components/pages/nonrecurring/SpendingBreakdown.tsx
+++ client/components/pages/nonrecurring/SpendingBreakdown.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
 import { Text, Box, Flex, Divider, Heading } from '@chakra-ui/react';
@@ -8,7 +8,7 @@ import { Text, Box, Flex, Divider, Heading } from '@chakra-ui/react';
 import { formatCurrency } from 'utils';
 import { Expense, CategoryObject } from 'utils/types';
 
-ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
+ChartJS.register(ArcElement, Tooltip, ChartDataLabels);
 
 interface SpendingBreakdownProps {
   expenses: Expense[];
@@ -27,59 +27,75 @@ export default function SpendingBreakdown(props: SpendingBreakdownProps) {
       >
         Expense Breakdown
       </Heading>
-      <Pie
-        data={{
-          labels: categories?.map((cat) => cat.name),
-          datasets: [
-            {
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              datalabels: { anchor: 'end' },
-              borderWidth: 1,
-            },
-          ],
-        }}
-        options={{
-          layout: { padding: 10 },
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            datalabels: {
-              backgroundColor: function (context) {
-                return context.dataset.borderColor as any;
-              },
-              borderColor: 'white',
-              borderWidth: 2,
-              color: 'white',
 
-              font: {
-                weight: 'bold',
-                size: 12,
+      <Text
+        fontSize="xs"
+        fontWeight="normal"
+        color="gray.500"
+        textAlign="center"
+        mt={1}
+      >
+        Hover over chart to view categories
+      </Text>
+
+      <Box px={4}>
+        <Pie
+          data={{
+            labels: categories?.map((cat) => cat.name),
+            datasets: [
+              {
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                datalabels: { anchor: 'end' },
+                borderWidth: 1,
               },
-              padding: 6,
-              formatter: (value) => {
-                return formatCurrency(value);
+            ],
+          }}
+          options={{
+            layout: { padding: 20 },
+            plugins: {
+              tooltip: {
+                callbacks: {
+                  label: (toolTipItem) =>
+                    `${toolTipItem.label}: ${formatCurrency(
+                      Number(toolTipItem.formattedValue)
+                    )}`,
+                },
+              },
+              datalabels: {
+                backgroundColor: function (context) {
+                  return context.dataset.borderColor as any;
+                },
+                borderColor: 'white',
+                borderWidth: 2,
+                color: 'white',
+                font: {
+                  weight: 'bold',
+                  size: 12,
+                },
+                formatter: (value) => {
+                  return formatCurrency(value);
+                },
               },
             },
-          },
-        }}
-      />
+          }}
+        />
+      </Box>
 
       <Box px={4}>
         {categories
