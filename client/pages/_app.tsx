73cbd2bc6ed8e6cba79de15d86cb1ca60001880e
francis <francis@aahcs.com>
2022-01-28 14:42:33 -0500

    fix: add progress loading bar when changing pages

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import TopBarProgress from 'react-topbar-progress-indicator';
 import '../styles/globals.css';
 
 import { RouteGuard } from 'components/RouteGuard';
@@ -10,7 +13,21 @@ import { theme } from 'theme';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: any) {
+  const router = useRouter();
+
+  const [progress, setProgress] = React.useState(false);
+
   const getLayout = Component.getLayout || ((page: any) => <>{page}</>);
+
+  // Function will fired when route change started
+  router.events?.on('routeChangeStart', () => {
+    setProgress(true);
+  });
+
+  // Function will fired when route change ended
+  router.events?.on('routeChangeComplete', () => {
+    setProgress(false);
+  });
 
   return (
     <>
@@ -22,7 +39,10 @@ function MyApp({ Component, pageProps }: any) {
 
       <QueryClientProvider client={queryClient}>
         <ChakraProvider theme={theme}>
-          <RouteGuard>{getLayout(<Component {...pageProps} />)}</RouteGuard>
+          <RouteGuard>
+            {progress && <TopBarProgress />}
+            {getLayout(<Component {...pageProps} />)}
+          </RouteGuard>
         </ChakraProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
